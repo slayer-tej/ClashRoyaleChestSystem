7f022050fa915be9878343786ba1d9a5b80c57fa
slayer-tej <aruntejagurrapu@gmail.com>
2021-06-18 21:58:15 +0530

    Added Controller for chests Generated in the slots which has status of chest generated in the slots like whether the chest is locked,isUnlocking or opened etc

---
+++ Assets/Scripts/ControllerChestLock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ControllerChestLock : MonoBehaviour
+{
+    public bool unlocked = false;
+    public bool isOpened = false;
+
+    public void Unlock()
+    {
+        unlocked = true;
+    }
+    public void Open(bool status)
+    {
+        isOpened = status;
+    }
+}
+
