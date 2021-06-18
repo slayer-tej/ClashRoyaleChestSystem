26aef35a2db09666f05d25c2651ee631206b1eea
slayer-tej <aruntejagurrapu@gmail.com>
2021-06-18 22:30:19 +0530

    added Onclick funtion for Button to Randomly generate script values

--- Assets/Scripts/ControllerChest.cs
+++ Assets/Scripts/ControllerChest.cs
@@ -1,13 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ControllerChest: MonoBehaviour
 {
+    [SerializeField] private Button _generatorChestButton;
+    private int _numOfSlots = 4;
+    [SerializeField]
     // Start is called before the first frame update
     void Start()
     {
-        
+        _generatorChestButton.onClick.AddListener(GenerateRandomChest);
+    }
+
+    private void GenerateRandomChest()
+    {
+        while(_numOfSlots <= 0)
+        {
+            int RandomChest = UnityEngine.Random.Range(-1, 4);
+            Debug.Log(RandomChest);
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +29,19 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
+}
+
+public struct Chest
+{
+    private ChestType Type;
+    private int Coins;
+    private int Gems;
+}
+
+public enum ChestType
+{
+    Common,
+    Rare,
+    Epic,
+    Legendary
 }
