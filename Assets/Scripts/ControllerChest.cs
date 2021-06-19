21018a7f46ccbfee7ff7e30776b2cf72d1745288
slayer-tej <aruntejagurrapu@gmail.com>
2021-06-19 22:04:58 +0530

    Added options to unlock individual chests when pressed

--- Assets/Scripts/ControllerChest.cs
+++ Assets/Scripts/ControllerChest.cs
@@ -1,19 +1,49 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ControllerChest : MonoBehaviour
 {
-    public bool locked = true;
-    public bool isOpened = false;
     [SerializeField]
-    private Button _chestButton;
+    private ChestType Type;
+    [SerializeField]
+    private int Coins;
+    [SerializeField]
+    private int Gems;
+    [SerializeField]
+    private int TimetoOpen;
+    public bool locked = true;
+    public bool isUnlocking = false;
+    public bool isCollected = false;
+    [SerializeField]
+    private Button _buttonChest;
     [SerializeField]
     private GameObject _optionsPanel;
+    [SerializeField]
+    private Button _buttonStartUnlocking;
+    [SerializeField]
+    private Button _buttonUnlockWithGems;
+
+
+    public void InitializeValues(Chest random)
+    {
+        Type = random.Type;
+        Coins = random.coins;
+        Gems = random.gems;
+        TimetoOpen = random.timetoOpen;
+    }
 
     private void Start()
     {
-        _chestButton.onClick.AddListener(OnClick);
+        _buttonChest.onClick.AddListener(OnClick);
+        _buttonStartUnlocking.onClick.AddListener(UnlockChest);
+    }
+
+    private void UnlockChest()
+    {
+        _optionsPanel.SetActive(false);
+        ServiceChestsSystem.Instance.StartUnlocking(this);
     }
 
     private void OnClick()
@@ -25,13 +55,24 @@ public class ControllerChest : MonoBehaviour
         }
     }
 
+    IEnumerator Countdown(int seconds)
+    {
+        int counter = seconds;
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1);
+            counter--;
+        }
+    }
+
     public void Unlock()
     {
         locked = false;
     }
+
     public void Open(bool status)
     {
-        isOpened = status;
+        isCollected = status;
     }
 }
 
--- Assets/Scripts/ServiceChestsSystem.cs
+++ Assets/Scripts/ServiceChestsSystem.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ServiceChestsSystem: MonoBehaviour
+public class ServiceChestsSystem: MonoSingletonGeneric<ServiceChestsSystem>
 {
     [SerializeField] private Button _generatorChestButton;
     private int _numOfSlots = 4;
     [SerializeField]
     private GameObject _chestPrefab;
+    [SerializeField]
+    private List<ControllerChest> chestsList = new List<ControllerChest>();
     // Start is called before the first frame update
     void Start()
     {
@@ -26,11 +28,26 @@ public class ServiceChestsSystem: MonoBehaviour
         }
     }
 
+    internal void StartUnlocking(ControllerChest controller)
+    {
+        foreach(ControllerChest chest in chestsList)
+        {
+            if(chest.GetInstanceID() == controller.GetInstanceID())
+            {
+            }
+        }
+    }
+
     private void LoadChest(int randomChest)
     {
-        Instantiate(_chestPrefab,gameObject.transform);
-        Chest chest = new Chest(randomChest);
-        Debug.Log("ChestType: " + chest.Type + " Coins: " + chest.coins + " Gems: "+chest.gems);
+        Chest Randomchest = new Chest(randomChest);
+        Debug.Log("ChestType: " + Randomchest.Type + " Coins: " + Randomchest.coins + " Gems: " + Randomchest.gems);
+        GameObject Loot =  Instantiate(_chestPrefab,gameObject.transform);
+        ControllerChest controllerChest = Loot.GetComponent<ControllerChest>();
+        controllerChest.InitializeValues(Randomchest);
+        chestsList.Add(controllerChest);
+        Debug.Log("Chest Added to List");
+        Debug.Log("Total Chests Added : " + chestsList.Count);
     }
 }
 
@@ -39,29 +56,35 @@ public struct Chest
     public ChestType Type;
     public int coins;
     public int gems;
+    public int timetoOpen;
 
     public Chest(int Randomnumber)
     {
         Type = (ChestType)Randomnumber;
         coins = 0;
         gems = 0;
+        timetoOpen = 0;
         switch (Type)
         {
             case ChestType.Common:
                 coins = UnityEngine.Random.Range(100, 201);
                 gems = UnityEngine.Random.Range(10, 21);
+                timetoOpen = 300;
                 break;
             case ChestType.Rare:
                 coins = UnityEngine.Random.Range(300, 501);
                 gems = UnityEngine.Random.Range(20, 41);
+                timetoOpen = 600;
                 break;
             case ChestType.Epic:
                 coins = UnityEngine.Random.Range(600, 801);
                 gems = UnityEngine.Random.Range(45, 61);
+                timetoOpen = 900;
                 break;
             case ChestType.Legendary:
                 coins = UnityEngine.Random.Range(1000, 1201);
                 gems = UnityEngine.Random.Range(80, 100);
+                timetoOpen = 1200;
                 break;
         }
     }
